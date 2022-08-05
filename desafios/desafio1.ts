5bf27ad03fe4087a545b58bf53de1608e92380c8
Julian Fontes S Vital <104820346+JuFSV@users.noreply.github.com>
2022-08-05 13:04:54 -0300

    enviando desafios

---
+++ desafios/desafio1.ts
@@ -0,0 +1,5 @@
+let employee: {name:string, code: number} = {
+  code: 10,
+  name: "John"
+
+}
---
+++ desafios/desafio2.ts
@@ -0,0 +1,29 @@
+interface dadosPessoa {
+  nome: string,
+  idade: number,
+  profissao: string
+}
+
+let pessoa1: dadosPessoa = {
+  nome: "Maria",
+  idade: 29,
+  profissao: "Atriz"
+}
+
+let pessoa2: dadosPessoa = {
+  nome: "Roberto",
+  idade: 19,
+  profissao: "Padeiro"
+}
+
+let pessoa3: dadosPessoa = {
+  nome: "Laura",
+  idade: 32,
+  profissao: "Atriz"
+}
+
+let pessoa4: dadosPessoa = {
+  nome: "Carlos",
+  idade: 19,
+  profissao: "Padeiro"
+}
